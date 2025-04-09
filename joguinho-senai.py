f25f78dc9aef62148039ff6422cbd14125286c48
mayconr4 <mayconhs2007@gmail.com>
2025-04-09 16:51:42 -0300

    em andamento projeto em pygame

--- joguinho-senai.py
+++ joguinho-senai.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 
 # Configuração inicial do pygame
 pygame.init()  # Inicia o jogo 
@@ -12,7 +13,17 @@ player_color = (255, 255, 0)  # Amarelo
 player_size = 50  # Tamanho do quadrado/player
 player_x = 100  # Posição inicial do quadrado/player
 player_y = 100  # Posição inicial do quadrado/player
-player_speed = 5  # Velocidade de movimentação
+player_speed = 5  # Velocidade de movimentação 
+
+player2_color = (255, 0, 0) 
+player2_size = 50  
+player2_x = 100
+player2_y = 100 
+player2_position = random.randint(1,1080)
+
+player1 = [player_color, player_size, player_x, player_y, player_speed]    
+
+player2 = [player2_color, player2_size, player2_x, player2_y,player2_position]
 
 # Loop principal do jogo
 while running:
@@ -30,16 +41,34 @@ while running:
     if keys[pygame.K_UP]:
         player_y -= player_speed  # Mover para cima
     if keys[pygame.K_DOWN]:
-        player_y += player_speed  # Mover para baixo
+        player_y += player_speed  # Mover para baixo 
+
+    if player1 in range(player2): 
+       contador = 0
+       while True: 
+           player1[1] = +20  
+           break
+       contador +=0
+
+    
+     
+
+
+
+    
 
     # Preenche o fundo com a cor azul
     screen.fill((0, 0, 255))
 
     # Desenha o personagem
     pygame.draw.rect(screen, player_color, (player_x, player_y, player_size, player_size))
+    pygame.draw.rect(screen,player2_color,(player2_x, player2_y,player_size,player_size))
+
 
     # Atualiza a tela
-    pygame.display.flip()
+    pygame.display.flip() 
+
+
 
     # Controla os FPS do jogo (60 FPS)
     clock.tick(60)
